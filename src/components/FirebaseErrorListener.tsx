7b17dcee828ab242e315468933d54a22bd266ed9
Yasir Khan <yasirwebio@gmail.com>
2025-10-03 04:46:29 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/components/FirebaseErrorListener.tsx
+++ src/components/FirebaseErrorListener.tsx
@@ -2,7 +2,7 @@
 import { useEffect } from 'react';
 import { errorEmitter } from '@/firebase/error-emitter';
 import { FirestorePermissionError } from '@/firebase/errors';
-import { useToast } from './ui/use-toast';
+import { useToast } from '@/hooks/use-toast';
 
 export function FirebaseErrorListener() {
   const { toast } = useToast();
